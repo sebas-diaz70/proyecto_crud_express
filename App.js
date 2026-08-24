756c71521e147c4723938d86842492dafb3ad4cb
sebastian_diaz <sebastian@suarez.com>
2026-08-24 11:12:06 -0500

    (commonjs) levantamiento del servidor Tradicional

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const app = express();
 const PUERTO = process.env.MIPUERTO || 3003;
 
 app.get('/', (req, res) => {
-    res.send('API Rest Full con express');
+    res.send('API Rest Full con expres');
 });
 
 app.listen(PUERTO, () => {
