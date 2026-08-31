19bd4fe43ce60166c53f3cbbec7d84c06e3f31f8
sebastian_diaz <sebastian@suarez.com>
2026-08-31 10:02:14 -0500

    añadimiento de datos json

--- App.js
+++ App.js
@@ -4,9 +4,45 @@ require('dotenv').config();
 const app = express();
 
 const PUERTO = process.env.MIPUERTO || 3003;
-
+//middleware body parser
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API Rest Full con expres');
+});
+
+app.get('/api/aprendices', (req, res) => {
+    res.status(200).json({ mensaje: 'Lista Aprendices' });
+});
+
+app.post('/api/aprendices', (req, res) => {
+    const datosAprendiz = req.body;
+    const edad=req.body.edad;
+   
+     if (edad >= 18) {
+
+        res.status(201).json({
+            mensaje: 'El aprendiz es mayor de edad',
+            datos: datosAprendiz
+        });
+
+    } else {
+
+        res.status(201).json({
+            mensaje: 'El aprendiz es menor de edad',
+            datos: datosAprendiz
+        });
+    };
+    res.status(201).json({ mensaje: 'crear aprendiz', 
+                            datos: datosAprendiz,
+                             edad: edad});
+});
+
+app.put('/api/aprendices/:id', (req, res) => {
+    res.status(200).json({ mensaje: 'actualizar aprendiz' });
+});
+
+app.delete('/api/aprendices/:id', (req, res) => {
+    res.status(200).json({ mensaje: 'eliminar aprendiz' });
 });
 
 app.listen(PUERTO, () => {
